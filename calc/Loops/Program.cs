6cfd5ee58562d42e595d6f2d11a7a45339591015
SupremeCom <aleksandr_chichkanov@mail.ru>
2021-05-28 12:53:23 +0700

    Add files via upload

---
+++ calc/Loops/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int i = 0;
+            //while (i < 5)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    if (i % 2 == 0)
+            //        Console.WriteLine(i);
+            //}
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    if (i % 2 != 0)
+            //        continue;
+            //        Console.WriteLine(i);
+            //}
+
+            //while (true)
+            //{
+            //    string str = Console.ReadLine();
+            //    if (Int32.Parse(str) > 0)
+            //        break;
+            //}
+
+            string str;
+            do
+            {
+                str = Console.ReadLine();    
+            }
+            while (Int32.Parse(str) <= 0);
+
+            
+
+
+
+        }
+    }
+}
---
+++ calc/calc/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string otv;
+           do
+            {
+                int x = GetNumber("Введите первое число");
+                int y = GetNumber("Введите второе число");
+
+                Console.WriteLine("Введите команду: +, -, *, /, min, max");
+                string cmd = Console.ReadLine();
+
+                int result = GetResult(x, y, cmd);
+
+                Console.WriteLine(result);
+                Console.WriteLine("Хотите ли вы продолжить вычисления? Введите команду: да, нет");
+                otv = Console.ReadLine();
+
+                
+                
+            }
+            while (otv == "да");
+        }
+
+        private static int GetResult(int x, int y, string cmd)
+        {
+            int result = 0;
+            switch (cmd)
+            {
+                case "+":
+                    result = x + y;
+                    break;
+                case "-":
+                    result = x - y;
+                    break;
+                case "*":
+                    result = x * y;
+                    break;
+                case "/":
+                    result = x / y;
+                    break;
+                case "max":
+                    result = GetMax(x,y);
+                    break;
+                case "min":
+                    result = GetMin(x, y);
+                    break;
+            }
+            return result;
+        }
+
+        private static int GetNumber(string text)
+        {
+            Console.WriteLine(text);
+            while (true)
+            {
+                string str = Console.ReadLine();
+                int result;
+                if (Int32.TryParse(str, out result))
+                    return result;
+                else
+                    Console.WriteLine("Введен не правильный формат числа");
+            }
+        }
+
+        static int GetMax(int a, int b)
+        {
+            int max = 0;
+            if (a > b)
+            {
+                max = a;
+            }
+            else
+            {
+                max = b;
+            }
+            return max;
+        }
+        static int GetMin(int a, int b)
+        {
+            int min = 0;
+            if (a < b)
+            {
+                min = a;
+            }
+            else
+            {
+                min = b;
+            }
+            return min;
+        }
+
+
+    }
+}
